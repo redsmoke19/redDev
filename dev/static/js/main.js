e105808b561ed14174c3d0a5091ddf8395239ba0
Oleg Tsyganov <TsyganovDev@gmail.com>
2020-08-19 23:20:55 +0400

    Дорабатывал настройки проекта

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,14 +1,15 @@
-(function() {
+(function () {
   // Sandwich
-  const getSandwichToggle = function() {
+  const getSandwichToggle = function () {
     const ENTER_BUTTON = 13;
     const sandwich = document.querySelector('.sandwich');
 
-    function sandwichOpen() {
+    function sandwichOpen () {
       sandwich.classList.toggle('sandwich-open');
     }
+
     sandwich.addEventListener('click', sandwichOpen);
-    sandwich.addEventListener('keydown', function(evt) {
+    sandwich.addEventListener('keydown', function (evt) {
       if (evt.keyCode === ENTER_BUTTON) {
         sandwichOpen();
       }
