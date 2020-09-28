379802a21f12a03ab39bff0f02dc565d57d7e6e7
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2020-09-28 15:29:52 +0400

    Finish styles and js for portfolio page + modal

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 (function() {
+  let ENTER__CODE = 13;
+  let ESC_CODE = 27;
   // Sandwich
   const getSandwichToggle = function() {
-    const ENTER_BUTTON = 13;
     let sandwich = document.querySelector('.sandwich');
     window.mainNav = document.querySelector('.main-nav');
     let navItems = Array.from(document.querySelectorAll('.main-nav__item'));
@@ -25,7 +26,7 @@
 
     sandwich.addEventListener('click', window.sandwichOpen);
     sandwich.addEventListener('keydown', function(evt) {
-      if (evt.keyCode === ENTER_BUTTON) {
+      if (evt.keyCode === ENTER__CODE) {
         window.sandwichOpen();
       }
     });
@@ -34,46 +35,69 @@
   const getPortfolioModal = function() {
     if (document.body.classList.contains('portfolio-page')) {
       let modal = document.querySelector('.modal-works');
-      let modalClose = document.querySelector('.modal-works__close');
+      let modalClose = document.querySelector('.modal-works__button');
       let previewsPicture = document.querySelectorAll('.works__item');
       let modalTitle = document.querySelector('.modal-works__title');
       let modalImage = document.querySelector('.modal-works__photo');
       let modalProjectName = document.querySelector('.js-project-name');
       let modalLink = document.querySelector('.js-project-site');
       let modalClient = document.querySelector('.js-project-client');
+      let overlay = document.querySelector('.overlay');
 
-      previewsPicture.forEach((item, index) => {
+      previewsPicture.forEach((item) => {
         item.addEventListener('click', () => {
-          modal.classList.add('modal-works--open');
-          document.body.classList.add('overflow-hidden');
-          let itemImage = item.querySelector('.works__photo');
-          modalTitle.textContent = itemImage.alt;
-          modalProjectName.textContent = ' ' + itemImage.alt;
-          modalImage.src = itemImage.src;
-          modalImage.srcset = itemImage.srcset;
-          modalImage.alt = itemImage.alt;
-          modalLink.textContent = itemImage.dataset.modalSite;
-          modalLink.href = itemImage.dataset.modalHref;
-          modalClient.textContent = ' ' + itemImage.dataset.modalClient;
+          getModalOpen(item);
+        });
+        item.addEventListener('keydown', (e) => {
+          if (e.keyCode === ENTER__CODE) {
+            getModalOpen(item);
+          }
         });
       });
+
+      let getModalOpen = function(item) {
+        let itemImage = item.querySelector('.works__photo');
+        modalTitle.textContent = itemImage.alt;
+        modalProjectName.textContent = ' ' + itemImage.alt;
+        modalImage.src = itemImage.src;
+        modalImage.srcset = itemImage.srcset;
+        modalImage.alt = itemImage.alt;
+        modalLink.textContent = itemImage.dataset.modalSite;
+        modalLink.href = itemImage.dataset.modalHref;
+        modalClient.textContent = ' ' + itemImage.dataset.modalClient;
+
+        modal.classList.add('modal-works--open');
+        document.body.classList.add('overflow-hidden');
+        if (window.matchMedia('(min-width: 992px)').matches) {
+          overlay.style.opacity = 1;
+          overlay.style.pointerEvents = 'all';
+        }
+        modalClose.addEventListener('click', getModalClose);
+        document.addEventListener('keydown', closeEcsModal);
+        overlay.addEventListener('click', closeClickOverlay);
+      };
 
       const getModalClose = function() {
         modal.classList.remove('modal-works--open');
         document.body.classList.remove('overflow-hidden');
+        overlay.style.opacity = 0;
+        overlay.style.pointerEvents = 'none';
+        document.removeEventListener('keydown', closeEcsModal);
+        overlay.removeEventListener('click', closeClickOverlay);
+        modalClose.removeEventListener('click', getModalClose);
       };
 
-      modalClose.addEventListener('click', getModalClose);
-      // modal.addEventListener('click', (e) => {
-      //   if (e.target.classList.contains('modal-works')) {
-      //     getModalClose();
-      //   }
-      // });
-      document.addEventListener('keydown', (e) => {
-        if (e.keyCode === 27) {
+      const closeClickOverlay = (e) => {
+        if (e.target.classList.contains('overlay')) {
           getModalClose();
         }
-      });
+      };
+
+      const closeEcsModal = (e) => {
+        if (e.keyCode === ESC_CODE) {
+          getModalClose();
+        }
+      };
     }
   };
   // Skills circle resize
